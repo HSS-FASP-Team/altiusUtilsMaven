a5512cb0d9acac4e899d40b3dc9c99dba76e16c4
Akil Mahimwala <akil.m@altius.cc>
2022-12-12 14:23:34 +0530

    Added functions to PassPhrase for getting OTP

--- src/main/java/cc/altius/utils/PassPhrase.java
+++ src/main/java/cc/altius/utils/PassPhrase.java
@@ -11,6 +11,7 @@ package cc.altius.utils;
 public final class PassPhrase {
 
     private static final int MIN_LENGTH = 8;
+    private static final int DEFAULT_OTP_LENGTH = 4;
     protected static java.util.Random r = new java.util.Random();
     protected static char[] goodChar = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
         'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
@@ -21,6 +22,38 @@ public final class PassPhrase {
     protected static char[] goodSmallCaseChar = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
         'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
         'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+    protected static char[] numberChar = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+    /**
+     * Get a random generated String that consists of digits only to be used for
+     * OTP
+     *
+     * @return Returns the random generated String of size 4 that consists of
+     * only digits
+     */
+    public static String getOtp() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < DEFAULT_OTP_LENGTH; i++) {
+            sb.append(numberChar[r.nextInt(numberChar.length)]);
+        }
+        return sb.toString();
+    }
+    
+    /**
+     * Get a random generated String that consists of digits only to be used for
+     * OTP
+     *
+     * @return Returns the random generated String of size 4 that consists of
+     * only digits
+     */
+    public static String getOtp(int length) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            sb.append(numberChar[r.nextInt(numberChar.length)]);
+        }
+        return sb.toString();
+    }
 
     /**
      * Get a random generated String that is only in small case or digits
