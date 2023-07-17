83534e87dcb7bf657025f5395a4db456a6f83242
Akil Mahimwala <akil.m@altius.cc>
2023-07-17 06:57:21 +0530

    Updated JSTL version

--- src/main/java/cc/altius/utils/SessionUtils.java
+++ src/main/java/cc/altius/utils/SessionUtils.java
@@ -4,8 +4,8 @@
  */
 package cc.altius.utils;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 /**
  *
